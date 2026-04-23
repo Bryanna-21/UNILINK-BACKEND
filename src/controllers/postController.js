d0f5370290b0c5e9ddfad72b2a83209a2065d62b
Bryanna-21 <bryannamanu@gmail.com>
2026-04-23 13:22:07 +0300

    Refactor postController to use database for posts

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,19 +1,30 @@
-let posts = [];
+const Post = require("../models/Post");
 
-exports.createPost = (req, res) => {
-  const { title, content } = req.body;
+exports.createPost = async (req, res) => {
+  const post = await Post.create({
+    userId: req.user.id,
+    universityId: req.user.universityId,
+    content: req.body.content
+  });
 
-  const newPost = {
-    id: Date.now(),
-    title,
-    content
-  };
-
-  posts.push(newPost);
-
-  res.json(newPost);
+  res.json(post);
 };
 
-exports.getPosts = (req, res) => {
+exports.getFeed = async (req, res) => {
+  const posts = await Post.find({})
+    .sort({ score: -1, createdAt: -1 })
+    .limit(50);
+
   res.json(posts);
+};
+
+exports.likePost = async (req, res) => {
+  const post = await Post.findById(req.params.id);
+
+  post.likes += 1;
+  post.score += 2;
+
+  await post.save();
+
+  res.json(post);
 };
