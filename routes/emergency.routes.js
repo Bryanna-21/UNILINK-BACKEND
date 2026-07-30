885aab1d8b4d3c99be9453498cbdca4719abbf09
Bryanna-21 <bryannamanu@gmail.com>
2026-07-30 16:47:16 +0300

    Delete routes directory

--- routes/emergency.routes.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const ctrl = require("../controllers/emergency.controller");
-const auth = require("../middleware/auth");
-
-router.post("/report", auth, ctrl.reportEmergency);
-router.get("/contacts", auth, ctrl.getContacts);
-router.post("/help", auth, ctrl.requestHelp);
-
-module.exports = router;
